9fcb0258632cb0884b470eb8e88bf532da8b7b55
Yash Dharmal <yashdharmal8353@gmail.com>
2022-11-10 11:36:57 +0530

    start script added

--- models/usersModel.js
+++ models/usersModel.js
@@ -27,7 +27,7 @@ userSchema.pre('save', async function (next) {
     try {
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(this.password, salt)
-        this.password = hashedPassword
+        this.password = hashedPassword;
         next()
     } catch (error) {
         next(error)
